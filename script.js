84c4a8ee72794755da5afd4406c0645ececdb69a
Ashish <ashishgupta57347@gmail.com>
2025-01-13 19:01:55 +0530

    DOM-1 Done

---
+++ script.js
@@ -0,0 +1,15 @@
+// 
+alert("Hello I'm Ashish Gupta");
+
+let div = document.getElementById("div");
+
+div.innerText = "Hello, I'm a div!"
+div.style.height = "200px";
+div.style.width = "300px";
+div.style.backgroundColor = "Yellow";
+div.style.color = "blue";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "Bold";
+
